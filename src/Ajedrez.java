944f1864fcd7b95e37443217ae415d39cd04a069
jorge15tr <57969214+jorge15tr@users.noreply.github.com>
2020-07-09 16:44:12 -0500

    terminado

---
+++ src/Ajedrez.java
@@ -0,0 +1,16 @@
+/*                        Integrantes
+Jorge Torres  |  Jose Sanchez  |  Daniel Barton  |  Arturo Naranjo
+ 8-960-2114   |   8-960-1339   |    8-961-138    |    8-947-1487
+ */
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class Ajedrez {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
